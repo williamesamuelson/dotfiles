fcce49a7cd6e2296a7ab33559922e788e35a6896
William Samuelson <william.samuelson@gmail.com>
2025-10-30 09:36:15 +0100

    latexmk build folder

--- config/nvim/lua/plugins/vimtex.lua
+++ config/nvim/lua/plugins/vimtex.lua
@@ -3,6 +3,9 @@ return {
     lazy = false,     -- we don't want to lazy load VimTeX
     init = function()
       -- VimTeX configuration goes here
-      vim.g.vimtex_view_method = "skim"
+        vim.g.vimtex_view_method = "skim"
+        vim.g.vimtex_compiler_latexmk = {
+            out_dir = 'build',
+        }
     end
 }
