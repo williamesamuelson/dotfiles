50c4c83282ac00e9273f04bca078858f89f33037
William Samuelson <william.samuelson@gmail.com>
2025-11-20 11:49:49 +0100

    disable copilot in tex and tmux colors

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -1,3 +1,13 @@
 return {
+  {
     'github/copilot.vim',
+    config = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "tex",
+        callback = function()
+          vim.cmd("Copilot disable")
+        end,
+      })
+    end,
+  },
 }
